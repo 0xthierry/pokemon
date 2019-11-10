02120c9fa90619e257b8fe3f13d1f5e847cea15d
Thierry Santos <thierry@dex.company>
2019-11-10 01:32:10 -0300

    feature(Sentry): error monitoring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
+
+Sentry.init({
+	dsn: process.env.REACT_APP_SENTRY,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
