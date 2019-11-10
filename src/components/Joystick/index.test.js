3813217176bc141cbe6d73dd0679713d2f50c17a
Thierry Santos <thierry@dex.company>
2019-11-10 08:03:12 -0300

    refactor(tests): remove duplication in tests

--- src/components/Joystick/index.test.js
+++ src/components/Joystick/index.test.js
@@ -4,35 +4,43 @@ import { render, fireEvent } from '@testing-library/react';
 import Joystick from '.';
 
 describe('Joystick component', () => {
+	let onClickMockFunction;
+	beforeEach(() => {
+		onClickMockFunction = jest.fn();
+	});
 	it('should call function onClickNext onClick', () => {
-		const onClickNext = jest.fn();
-		const { getByTestId } = render(<Joystick onClickNext={onClickNext} />);
+		const { getByTestId } = render(
+			<Joystick onClickNext={onClickMockFunction} />
+		);
 		fireEvent.click(getByTestId('joystick-next'));
 
-		expect(onClickNext.mock.calls.length).toBe(1);
+		expect(onClickMockFunction.mock.calls.length).toBe(1);
 	});
 
 	it('should call function onClickBack onClick', () => {
-		const onClickBack = jest.fn();
-		const { getByTestId } = render(<Joystick onClickBack={onClickBack} />);
+		const { getByTestId } = render(
+			<Joystick onClickBack={onClickMockFunction} />
+		);
 		fireEvent.click(getByTestId('joystick-back'));
 
-		expect(onClickBack.mock.calls.length).toBe(1);
+		expect(onClickMockFunction.mock.calls.length).toBe(1);
 	});
 
 	it('should call function onClickDown onClick', () => {
-		const onClickDown = jest.fn();
-		const { getByTestId } = render(<Joystick onClickDown={onClickDown} />);
+		const { getByTestId } = render(
+			<Joystick onClickDown={onClickMockFunction} />
+		);
 		fireEvent.click(getByTestId('joystick-down'));
 
-		expect(onClickDown.mock.calls.length).toBe(1);
+		expect(onClickMockFunction.mock.calls.length).toBe(1);
 	});
 
 	it('should call function onClickUp onClick', () => {
-		const onClickUp = jest.fn();
-		const { getByTestId } = render(<Joystick onClickUp={onClickUp} />);
+		const { getByTestId } = render(
+			<Joystick onClickUp={onClickMockFunction} />
+		);
 		fireEvent.click(getByTestId('joystick-up'));
 
-		expect(onClickUp.mock.calls.length).toBe(1);
+		expect(onClickMockFunction.mock.calls.length).toBe(1);
 	});
 });
